c4dbba49894cd7e8011205df4bb6649a837a5baf
ZEINZH <91441817+suryanicholas@users.noreply.github.com>
2023-05-02 08:35:53 +0700

    cHNAGED

---
+++ zlibraries/deMyJS/if-statement.js
@@ -0,0 +1,46 @@
+// Operator Logika AND (&&)
+// Operator Logika OR (||)
+// Operator Logika NOT (!)
+// Operator Logika XOR (^)
+
+// Operator Aritmetika
+var a = 10;
+var b = 5;
+// Penjumlahan
+var c = a + b; // hasilnya adalah 15
+// Pengurangan
+var d = a - b; // hasilnya adalah 5
+// Perkalian
+var e = a * b; // hasilnya adalah 50
+// Pembagian
+var f = a / b; // hasilnya adalah 2
+// Modulus
+var g = a % b; // hasilnya adalah 0
+// Increment
+var h = a++; // hasilnya adalah 10
+var i = ++b; // hasilnya adalah 6
+// Decrement
+var j = a--; // hasilnya adalah 11
+var k = --b; // hasilnya adalah 5
+
+// Operator Perbandingan
+// Operator Sama Dengan (=)
+// Operator Tidak Sama Dengan (!=)
+// Operator lebih besar dari (>)
+// Operator kurang dari (<)
+// Operator kurang atau sama dengan (<=)
+// Operator lebih atau sama dengan (>=)
+var a = 10;
+var b = 5;
+
+if (a == b) {
+  console.log("a sama dengan b");
+}
+
+if (a > b) {
+  console.log("a lebih besar dari b");
+}
+
+if (a != b) {
+  console.log("a tidak sama dengan b");
+}

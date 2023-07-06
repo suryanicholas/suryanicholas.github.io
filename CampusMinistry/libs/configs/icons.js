b3bd68f5c83f376f814a3a96cabcccdf226dcff0
ZEINZH <91441817+suryanicholas@users.noreply.github.com>
2023-07-06 15:20:53 +0700

    Update New Version

--- CampusMinistry/libs/configs/icons.js
+++
@@ -1,6 +0,0 @@
-const thisHead = document.querySelector('head');
-const thisFavicon = document.createElement('link');
-thisFavicon.rel = 'icon';
-thisFavicon.type = 'image/x-icon';
-thisFavicon.href='libs/assets/logo.png';
-thisHead.appendChild(thisFavicon);
--- zlibraries/deMyJS/services-genshin.js
+++
@@ -1,8 +0,0 @@
-const getCanvasHeader = document.querySelector('.canvas-header');
-window.addEventListener('scroll', function() {
-    if (this.window.pageYOffset > 75) {
-        getCanvasHeader.classList.add('scrolled');
-    } else {
-        getCanvasHeader.classList.remove('scrolled');
-    }
-});

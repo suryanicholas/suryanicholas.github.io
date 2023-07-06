910e266909621c397800e65036e14866c0285c32
ZEINZH <91441817+suryanicholas@users.noreply.github.com>
2023-07-06 15:31:27 +0700

    New

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Surya Nicholas</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    
+</body>
+</html>
